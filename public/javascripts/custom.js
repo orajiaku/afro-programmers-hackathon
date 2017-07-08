ff9a3667a6116d27a0085fdf63f4a420e147a388
Orajiaku Chukwudalu <chukwudalurjk@gmail.com>
2017-07-08 13:56:28 -0400

    added comments view

---
+++ public/javascripts/custom.js
@@ -0,0 +1,28 @@
+/**
+ * Created by orajiakuchukwudalu on 2017-07-08.
+ */
+'use strict';
+
+(function() {
+
+})();
+
+function onCommentClick() {
+
+}
+
+function onSubmitComment() {
+
+}
+
+function hideComments() {
+    $( ".commentsView" ).hide();
+}
+
+function showComments() {
+    $( ".commentsView" ).show();
+}
+
+function appendComment() {
+
+}
