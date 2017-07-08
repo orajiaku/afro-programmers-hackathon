a303521c3dbbf6085a6725a55dff16ce134487fd
jcchuks <jcchuks@ymail.com>
2017-07-08 15:56:01 -0400

    Updates query.js

--- afro-programmers-hackathon/query/query.js
+++ afro-programmers-hackathon/query/query.js
@@ -2,15 +2,27 @@ var globals = require("../../globals");
 var io = require(globals.vars.app).io;
 var storeHandler = require(globals.vars.store).db;
 
-var MSGTYPES = {DRAW:"DRAW",DRAWREQUEST:"DREQ", NEW:"NEWRM"}
+var MSGTYPES = {DRAW:"DRAW",
+		DRAWREQUEST:"DREQ", 
+		NEW:"NEWRM", 
+		GETHISTORY:"GETHIST"
+	}
 
 var getDrawingHistoryForNewClients = function (socket_id, room,) {
 	 /* body... */ 
 	  io.sockets.socket(storeHandler.storage[room_name].admin).emit("ToBeAgreedUpon", {type:DRAWREQUEST, msg: "Think"});
+	  //io.
 }
 
 var sendErrorPage = function (argument) {
 	// body... 
+}
+
+var broadCastDrawing = function (draw_parameters, room_name) {
+	// We have to agree on how to call getDrawing
+
+	socket_id.broadcast.to(room_name).emit('ToBeAgreedUpon', { type:MSGTYPES.DRAW, 
+					msg: draw_parameters });
 }
 
 var getDrawing = function (socket_id) {
@@ -19,14 +31,8 @@ var getDrawing = function (socket_id) {
 	socket_id.on('ToBeAgreedUpon', function(data) {
 		draw_parameters = data;
     })
-	return draw_parameters;
-}
-
-var broadCastDrawing = function (draw_parameters, socket_id, room_name) {
-	// We have to agree on how to call getDrawing
-
-	socket_id.broadcast.to(room_name).emit('ToBeAgreedUpon', { type:MSGTYPES.DRAW, 
-					msg: draw_parameters });
+    broadCastDrawing(draw_parameters, getRoomName(socket_id));
+	//return draw_parameters;
 }
 
 var createRoom = function (socket_id, password, room_name) {
@@ -40,14 +46,31 @@ var createRoom = function (socket_id, password, room_name) {
 
 var joinRoom = function (socket_id, room_name) {
 	/* body... */
+	if(checkRoom(room_name)){
+		storeHandler.storage[room_name].users.push(room_name);
+	}
+
+}
+
+var getRoomName = function (socket_id) {
+	var room_name = socket_id.rooms[Object.keys(socket_id.rooms)[0]];//returns name of room
+	return room_name
 }
 
 var checkRoom = function (room_name) {
 	 return room_name in storeHandler.storage;
 }
 
-var onDisconnect = function (socket_id) {
+var onDisconnect = function (socket_id, room_name) {
 	 /* body... */ 
+	 socket_id.leave(room_name);
+	 if(storeHandler.storage[room_name].admin == socket_id){
+	 	promoteSomeoneInRoom(room_name);
+	 }
+	 var index = storeHandler.storage[room_name].users.indexOf(socket_id)
+	 if (index > 0){ 
+	 	storeHandler.storage[room_name].users.splice(index, 1);
+	 }
 }
 
 var promoteSomeoneInRoom = function (room_name) {
