749e7c39f57f170847861023da1a19272f645d2b
jcchuks <jcchuks@ymail.com>
2017-07-09 00:20:06 -0400

    Query update with io

--- afro-programmers-hackathon/query/query.js
+++ afro-programmers-hackathon/query/query.js
@@ -5,8 +5,15 @@ var storeHandler = require(globals.vars.store).db;
 var MSGTYPES = {DRAW:"DRAW",
 		DRAWREQUEST:"DREQ", 
 		NEW:"NEWRM", 
-		GETHISTORY:"GETHIST"
+		COMMENT:"CMT",
+		GETHISTORY:"GETHIST",
+		JOINRM:"JOIN"
 	}
+
+var EVENTS = {COMMENT:"comment",
+			DRAWING:"drawing",
+			JOINRM:"join_room",
+			CREATERM:"create_room"}
 
 var getDrawingHistoryForNewClients = function (socket_id, room) {
 	 /* body... */ 
@@ -18,20 +25,19 @@ var sendErrorPage = function (argument) {
 	// body... 
 }
 
-var broadCastDrawing = function (draw_parameters, room_name) {
+var broadCastMsg = function ( socket_id, room_name, event, msgType, msg) {
 	// We have to agree on how to call getDrawing
-
-	socket_id.broadcast.to(room_name).emit('ToBeAgreedUpon', { type:MSGTYPES.DRAW, 
-					msg: draw_parameters });
+	socket_id.broadcast.to(room_name).emit(event, { type:msgType, msg: msg });
 }
 
-var getDrawing = function (socket_id) {
-	/* body... */
-	var draw_parameters = null;
-	socket_id.on('ToBeAgreedUpon', function(data) {
-		draw_parameters = data;
-    })
-    broadCastDrawing(draw_parameters, getRoomName(socket_id));
+var sendComments = function (socket_id, comment) {
+	// We have to agree on how to call getDrawing
+	broadCastMsg(socket_id, getRoomName(socket_id),EVENTS.COMMENT,MSGTYPES.COMMENT,comment); 
+}
+
+var sendDrawing = function (socket_id,draw_parameters) {
+	/* body... */ 
+    broadCastMsg(socket_id, getRoomName(socket_id),EVENTS.DRAWING,MSGTYPES.DRAW,draw_parameters);
 	//return draw_parameters;
 }
 
@@ -41,13 +47,19 @@ var createRoom = function (socket_id, password, room_name) {
 	}else{
 		sendErrorPage()
 	}
-	socket_id.emit("ToBeAgreedUpon",{type:MSGTYPES.NEW,msg:""})
+	socket_id.emit("room_created",{type:MSGTYPES.NEW,msg:"Room Created"})
 }
 
 var joinRoom = function (socket_id, room_name) {
 	/* body... */
 	if(checkRoom(room_name)){
 		storeHandler.storage[room_name].users.push(room_name);
+		broadCastMsg(socket_id, room_name ,EVENTS.JOINRM ,MSGTYPES.JOINRM,"New User Joined");
+		socket.join(room_name, function(){
+				console.log('Joined default room');
+				});
+	}else {
+		createRoom(socket_id,null, room_name);
 	}
 
 }
@@ -92,20 +104,25 @@ module.exports = {
 
 		io.on('connection', function(socket){
 			console.log('Endpoint connected');
-
-			socket.join('default room', function(){
-				console.log('Joined default room');
-
-				socket.on('comment', function(data){
-					socket.to('default room').emit('comment', data);
-					socket.emit('comment', data);
+			socket.on('join_room', function () {
+				/* body... */
+				joinRoom(socket, data)
+			})
+			
+			socket.on(EVENTS.COMMENT, function(data){ 
+					sendComments(socket, data);
 				});
 				
-				socket.on('drawing', function(data){
-					socket.to('default room').emit('drawing', data);
-					socket.emit('drawing', data);
+			socket.on(EVENTS.DRAWING, function(data){
+					sendDrawing(socket, data);
+					/*socket.to('default room').emit('drawing', data);
+					socket.emit('drawing', data);*/
 				});
-			})
+
+			socket.on(EVENTS.CREATERM, function (data) {
+					createRoom(socket, data.password || null, data.room_name);
+				});
+			 
 		});
 	},
 	createRoom: createRoom,
